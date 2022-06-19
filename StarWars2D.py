f791ac9378e4aeef9dc42648cf340c803efbeae7
Arian Barilli <arianbarilli@gmail.com>
2022-06-19 16:50:29 -0300

    Melhorando a Jogabilidade V.3

--- StarWars2D.py
+++ StarWars2D.py
@@ -1,49 +1,52 @@
 import pygame
 import random
 pygame.init()
-largura = 1520
-altura = 770
+largura = 1530
+altura = 780
 tamanho = (largura, altura)
 pygameDisplay = pygame.display
 pygameDisplay.set_caption("Star Wars")
 gameDisplay = pygame.display.set_mode(tamanho)
-gameIcon = pygame.image.load("assets/R2D2.ico")
+gameIcon = pygame.image.load("assets/Vader.ico")
 pygameDisplay.set_icon(gameIcon)
 
-fundo = pygame.image.load("assets/fundo.jpg")
-fundoDeath = pygame.image.load("assets/navedestroyed.jpg")
+fundo = pygame.image.load("assets/fundo.jpeg")
+fundoDeath = pygame.image.load("assets/navedestroyed.jpeg")
 
 explosaoSound = pygame.mixer.Sound("assets/explode.ogg")
 explosaoSound.set_volume(0.5)
-yellow = (255, 255, 0)
+red = (255, 0, 0)
 green = (0, 255, 0)
 clock = pygame.time.Clock()
 eventos = pygame.event
 
-def dead(pontos):
+def game_over(pontos):
     gameDisplay.blit(fundoDeath, (0, 0))
     pygame.mixer.music.stop()
     pygame.mixer.Sound.play(explosaoSound)
     fonte = pygame.font.Font("freesansbold.ttf", 50)
     fonteContinue = pygame.font.Font("freesansbold.ttf", 30)
-    texto = fonte.render("Você Perdeu com "+str(pontos) +
-                         " pontos", True, yellow)
-    textoContinue = fonteContinue.render(
-        "Aperte ENTER para continuar...", True, green)
-    gameDisplay.blit(textoContinue, (200, 700))
-    gameDisplay.blit(texto, (200, 600))
+    texto = fonte.render("Você Perdeu com "+str(pontos) + " pontos", True, red)
+    textoContinue = fonteContinue.render("Aperte ENTER para continuar...", True, green)
+    gameDisplay.blit(textoContinue, (120, 700))
+    gameDisplay.blit(texto, (120, 600))
     pygameDisplay.update()
 
 def jogo():
+    direcaoNave = False
     pontos = 0
-    posicaoXLaser = 0
+    posicaoXLaser = -175
+    posicaoXLaser2 = 1530
     posicaoYLaser = random.randrange(0, altura-120)
+    posicaoYLaser2 = random.randrange(0, altura-120)
     direcao = True
     speed = 2
     posicaoXNave = 650
     posicaoYNave = 300
-    movimentoXNave = 0
-    movimentoYNave = 0
+    movimentoANave = 0
+    movimentoDNave = 0
+    movimentoWNave = 0
+    movimentoSNave = 0
 
     laser = pygame.image.load("assets/laser.png")
     nave = pygame.image.load("assets/nave.png")
@@ -60,7 +63,7 @@ def jogo():
     alturaNave = 150
     larguraMissel = 220
     alturaMissel = 120
-    dificuldade = 63
+    dificuldade = 59
     jogando = True
 
     while True:
@@ -72,21 +75,36 @@ def jogo():
                 if event.key == pygame.K_RETURN:
                     jogo()
                 if event.key == pygame.K_a:
-                    movimentoXNave = -25
+                    movimentoANave = -25
                 elif event.key == pygame.K_d:
-                    movimentoXNave = 25
+                    movimentoDNave = 25
                 elif event.key == pygame.K_w:
-                    movimentoYNave = -25
+                    movimentoWNave = -25
                 elif event.key == pygame.K_s:
-                    movimentoYNave = 25
+                    movimentoSNave = 25
             if event.type == pygame.KEYUP:
-                if event.key == pygame.K_a or event.key == pygame.K_d or event.key == pygame.K_w or event.key == pygame.K_s:
-                    movimentoXNave = 0
-                    movimentoYNave = 0
+                if event.key == pygame.K_a:
+                    movimentoANave = 0
+                elif event.key == pygame.K_d:
+                    movimentoDNave = 0
+                elif event.key == pygame.K_w:
+                    movimentoWNave = 0
+                elif event.key == pygame.K_s:
+                    movimentoSNave = 0
+            
+            if direcaoNave == True and movimentoANave + movimentoDNave < 0:
+                nave = pygame.transform.flip(nave, True, False)
+                direcaoNave = False
+            elif direcaoNave == False and movimentoDNave + movimentoDNave > 0:
+                nave = pygame.transform.flip(nave, True, False)
+                direcaoNave = True
+
 
         if jogando == True:
-            posicaoXNave = posicaoXNave + movimentoXNave
-            posicaoYNave = posicaoYNave + movimentoYNave
+            posicaoXNave = posicaoXNave + movimentoANave
+            posicaoXNave = posicaoXNave + movimentoDNave
+            posicaoYNave = posicaoYNave + movimentoWNave
+            posicaoYNave = posicaoYNave + movimentoSNave
             if posicaoXNave < 0:
                 posicaoXNave = 0
             elif posicaoXNave >= largura - larguraNave:
@@ -97,11 +115,8 @@ def jogo():
             elif posicaoYNave >= altura - alturaNave:
                 posicaoYNave = altura - alturaNave
 
-
-            gameDisplay.blit(fundo, (0, 0))
-
             if direcao == True:
-                if posicaoXLaser < largura -0:
+                if posicaoXLaser < largura:
                     posicaoXLaser = posicaoXLaser + speed
                 else:
                     pygame.mixer.Sound.play(laserSound)
@@ -111,7 +126,7 @@ def jogo():
                     laser = pygame.transform.flip(laser, True, False)
                     pontos = pontos + 1
             else:
-                if posicaoXLaser >= -150:
+                if posicaoXLaser >= -220:
                     posicaoXLaser = posicaoXLaser - speed
                 else:
                     pygame.mixer.Sound.play(laserSound)
@@ -121,26 +136,45 @@ def jogo():
                     laser = pygame.transform.flip(laser, True, False)
                     pontos = pontos + 1
 
+            if direcao == True and pontos >= 20:
+                if posicaoXLaser2 < largura:
+                    posicaoXLaser2 = posicaoXLaser2 + speed
+                else:
+                    posicaoYLaser2 = random.randrange(0, altura -120)    
+
+            elif direcao == False and pontos >= 20:
+                if posicaoXLaser2 >= -220:
+                    posicaoXLaser2 = posicaoXLaser2 - speed
+                else:
+                    posicaoYLaser2 = random.randrange(0, altura -120)
+                    
+            gameDisplay.blit(fundo, (0, 0))
             gameDisplay.blit(laser, (posicaoXLaser, posicaoYLaser))
+            if pontos >= 20:
+                gameDisplay.blit(laser, (posicaoXLaser2, posicaoYLaser2))
             gameDisplay.blit(nave, (posicaoXNave, posicaoYNave))
             fonte = pygame.font.Font("freesansbold.ttf", 20)
             placar = fonte.render("Pontos: "+str(pontos), True, green)
             gameDisplay.blit(placar, (10, 10))
 
-            pixelsYNave = list(
-                range(posicaoYNave, posicaoYNave + alturaNave+1))
-            pixelsXNave = list(
-                range(posicaoXNave, posicaoXNave + larguraNave+1))
+            pixelsYNave = list(range(posicaoYNave, posicaoYNave + alturaNave+1))
+            pixelsXNave = list(range(posicaoXNave, posicaoXNave + larguraNave+1))
 
-            pixelsYMissel = list(range(posicaoYLaser, posicaoYLaser+alturaMissel+1))
-            pixelsXMissel = list(range(posicaoXLaser, posicaoXLaser+larguraMissel+1))
+            pixelsYMissel = list(range(posicaoYLaser, posicaoYLaser + alturaMissel+1))
+            pixelsXMissel = list(range(posicaoXLaser, posicaoXLaser + larguraMissel+1))
 
+            pixelsYMissel2 = list(range(posicaoYLaser2, posicaoYLaser2 + alturaMissel+1))
+            pixelsXMissel2 = list(range(posicaoXLaser2, posicaoXLaser2 + larguraMissel+1))
+        
             if len(list(set(pixelsYMissel) & set(pixelsYNave))) > dificuldade:
                 if len(list(set(pixelsXMissel) & set(pixelsXNave))) > dificuldade:
                     jogando = False
-                    dead(pontos)
+                    game_over(pontos)
+            if len(list(set(pixelsYMissel2) & set(pixelsYNave))) > dificuldade:
+                if len(list(set(pixelsXMissel2) & set(pixelsXNave))) > dificuldade:
+                    jogando = False
+                    game_over(pontos)
 
         pygameDisplay.update()
         clock.tick(60)
-
 jogo()
